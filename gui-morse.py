7b120b1a3a257b1729f4d7b03d8dc934f6600fc9
arish00100 <khanarish02@gmail.com>
2021-08-30 06:16:32 +0530

    2nd prototype

--- gui-morse.py
+++ gui-morse.py
@@ -1,5 +1,6 @@
 import tkinter as tk
-from tkinter import font
+from tkinter import Variable, font
+from tkinter.constants import FLAT, SUNKEN
 import morsecode,time
 
 
@@ -7,7 +8,8 @@ import morsecode,time
 #Initial setup
 
 win=tk.Tk()
-win.geometry('800x800')
+win.geometry('800x780+300+0')
+win.resizable(False,False)
 win.title("Morse Code Translator")
 win.configure(bg='gray15')
 
@@ -54,13 +56,14 @@ header_frame=tk.Frame(bg='gray15')
 
 #Heading
 header=tk.Label(header_frame,text="Morse Code translator",font=('Source Code Pro Semibold',30),
-                bg='gray15',fg='steelblue1'
+                bg='gray15',fg='#f20d0d'
                 )
 header.pack()
 
 #Morse design
-design=tk.Label(header_frame,text='-- --- .-. ... . / -.-. --- -.. .',bg='gray15',fg='red',
-                font=('Source Code Pro Semibold',20),)
+design=tk.Label(header_frame,text='-- --- .-. ... . / -.-. --- -.. .',bg='gray15',fg='#f20d0d',
+                font=('Source Code Pro Semibold',20),
+                )
 design.pack()
 
 header_frame.pack(pady=20)
@@ -70,26 +73,31 @@ header_frame.pack(pady=20)
 #User entry
 
 #Text entry frame
-user_entry_frame=tk.Frame()
+user_entry_frame=tk.Frame(bg='gray15')
 
 #Text entry label
-user_entry_label=tk.Label(text="Enter text",master=user_entry_frame,bg='gray15',
-                fg='steelblue1',width=50,anchor='w'
+user_entry_label=tk.Label(user_entry_frame,text="Enter text",bg='gray15',
+                fg='#f20d0d',width=50,anchor='w'
                 )
-user_entry_label.pack(fill=tk.X)
+user_entry_label.pack()
 
 #Text entry
-user_entry=tk.Entry(textvariable=user_text,master=user_entry_frame,font=("Helvetica",15)
-                    ,width=50,bg='black',fg='white')
-user_entry.pack(fill=tk.Y)
+user_entry=tk.Entry(user_entry_frame,textvariable=user_text,
+                    width=50,bg='#D0D0D0',fg='black',relief=tk.FLAT,bd=2
+                    )
+user_text.set('Enter text here')
+user_entry.pack()
 
 user_entry_frame.pack(pady=30)
 
 #########################
 #Button to encrypt
 
-encrypt_button=tk.Button(text='Translate to morse',command=encrypt,bg='firebrick4',fg='white')
-encrypt_button.pack()
+encrypt_button=tk.Button(text='Translate to morse',command=encrypt,bg='#a11212',fg='white',
+                activebackground='#a11212',activeforeground='white',
+                relief=tk.FLAT,bd=3
+                )
+encrypt_button.pack(pady=10)
 
 #########################
 #Translated morse
@@ -98,21 +106,22 @@ encrypt_button.pack()
 morse_code_frame=tk.Frame(bg='gray15')
 
 #Morse code label
-morse_code_label=tk.Label(master=morse_code_frame,text="Morse code",bg='gray15',fg='steelblue1',
-                anchor='w'
+morse_code_label=tk.Label(morse_code_frame,text="Morse code",bg='gray15',fg='#f20d0d',
+                width=50,anchor='w'
                 )
-morse_code_label.pack(fill=tk.X)
+morse_code_label.pack()
 
 #Translated morse
-morse_entry=tk.Entry(master=morse_code_frame,font=("Helvetica",15),textvariable=translated_morse,
-                    width=50,bg='black',fg='white'
+morse_entry=tk.Entry(morse_code_frame,textvariable=translated_morse,
+                    width=50,bg='#D0D0D0',fg='black',relief=tk.FLAT,bd=2,
                     )
 morse_entry.pack()
 
 #Button to play sound
-beep_button=tk.Button(master=morse_code_frame,text='Play sound',command=playsound,bg='firebrick4',fg='white',
-            width=10)
-beep_button.pack(pady=10,anchor='e')
+beep_button=tk.Button(morse_code_frame,text='Play sound',command=playsound,bg='#a11212',fg='white',
+            activebackground='#a11212',activeforeground='white', relief=tk.FLAT, bd=3
+            )
+beep_button.pack(pady=5,anchor='e')
 
 morse_code_frame.pack(pady=30)
 
@@ -124,21 +133,23 @@ sliders_frame=tk.Frame(bg='gray15')
 
 #Time unit scale
 timeunit_scale=tk.Scale(sliders_frame,orient=tk.HORIZONTAL,from_=0.1,to=1,resolution=0.1,
-                variable=timeunit_var,bg='gray15',fg='steelblue1',length=500,label="Set time unit (seconds)",
-                relief=tk.FLAT,bd=0,highlightthickness=0
+                variable=timeunit_var,bg='gray15',fg='#f20d0d',length=500,label="Set time unit (seconds)",
+                relief=tk.FLAT,bd=0,highlightthickness=0,troughcolor='#D0D0D0',font=("Segoe UI Semibold",12),
+                activebackground='#a11212'
                 )
 timeunit_scale.set(0.2)
 timeunit_scale.pack(pady=10)
 
 #Frequency scale
 frequency_scale=tk.Scale(sliders_frame,orient=tk.HORIZONTAL,from_=100,to=3000,resolution=100,
-                variable=frequency_var,bg='gray15',fg='steelblue1',length=500,label="Set frequency (Hz)",
-                highlightthickness=0
+                variable=frequency_var,bg='gray15',fg='#f20d0d',length=500,label="Set frequency (Hz)",
+                highlightthickness=0,troughcolor='#D0D0D0',font=("Segoe UI Semibold",12),
+                activebackground='#a11212'
                 )
 frequency_scale.set(1000)
 frequency_scale.pack(pady=10)
 
-sliders_frame.pack()
+sliders_frame.pack(pady=30)
 
 
 
@@ -157,15 +168,11 @@ win.mainloop()
 
 #Widgets javatpoint
 #Borders
-#relief
-#bd
 #All widgets functions attributes
 #Layout function attributes
 
-#Colors and font
-#Color theme
-#Entry color
-#UI primary and secondary colors
+
+#Button focus colors
 
 #Text to speech
 #pyttsx3
@@ -181,6 +188,10 @@ win.mainloop()
 #Only for windows
 
 #error handling
+
+#translator code modification -gfg
+
+#icon - red shaded png
 
 
 
